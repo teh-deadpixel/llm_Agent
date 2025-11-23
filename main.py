86373864251e9b39c88d0809288cbf85c6e4c823
teh-deadpixel <shroffharsh2@gmail.com>
2025-11-23 11:56:20 +0000

    added a way to get function call details and stuff

--- main.py
+++ main.py
@@ -19,6 +19,33 @@ def main():
         sys.exit(1)
     client = genai.Client(api_key = api_key)
     verbose = "--verbose" in sys.argv
+    schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
+    available_functions = types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
+    system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
     prompt = sys.argv[1]
     messages = [types.Content(role="user", parts=[types.Part(text=prompt)]),]
     if verbose :
@@ -27,9 +54,14 @@ def main():
         response = client.models.generate_content(
             model="gemini-2.0-flash-001",
             contents=messages,
+            config=types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt)
         )
-        print(response.text)
         
+        if response.function_calls == None:
+            print(response.text)
+        else:
+            for function_call_part in response.function_calls:
+                print(f"Calling function: {function_call_part.name}({function_call_part.args})")
             
         if verbose and hasattr(response, "usage_metadata") and response.usage_metadata:
             
